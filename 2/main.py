5095383b1f6a2287bb856d446e876686aaf3ec0e
Mksym Bondarenko <maksym.bondarenko@eliftech.com>
2025-02-23 12:33:32 +0000

    Added user interaction

--- 2/main.py
+++ 2/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import inquirer
 
 # Load the JSON file
 file_path = "openaq.json"
@@ -10,18 +11,43 @@ df = pd.json_normalize(data)
 # Filter columns and set index
 df = df[["country_name_en","city", "location", "measurements_parameter", "measurements_value", "measurements_lastupdated"]]
 df["measurements_lastupdated"] = pd.to_datetime(df["measurements_lastupdated"], utc=True)
+
+# Extract indexed field values
+pollutants = df["measurements_parameter"].unique().tolist()
+countries = df["country_name_en"].unique().tolist()
+
+# Set indexes
 df.set_index(["country_name_en", "measurements_parameter"], inplace=True)
 df.to_csv("results/Dataset.csv")
 
-# Queries using xs
-country_data = df.xs("India", level="country_name_en")
-country_data.to_csv("results/India.csv")
-print("Stored query result")
+# Prompt user
+while True:
+    questions = [
+        inquirer.List(
+            "pollutant",
+            message="Select a pollutant to search by",
+            choices=['Skip', 'Exit', *pollutants],
+        ),
+        inquirer.List(
+            "country",
+            message="Select a country to search by",
+            choices=['Skip', 'Exit', *countries],
+        )
+    ]
+    answers = inquirer.prompt(questions)
+    selected_pollutant = answers["pollutant"]
+    selected_country = answers["country"]
 
-polutant_data = df.xs("CO", level="measurements_parameter")
-polutant_data.to_csv("results/CO.csv")
-print("Stored query result")
+    if selected_pollutant == 'Exit' or selected_country == 'Exit': exit(0)
 
-composite_data = df.xs(("India", "CO"), level=("country_name_en", "measurements_parameter"))
-composite_data.to_csv("results/India_CO.csv")
-print("Stored query result")
+    # Search
+    if selected_country != 'Skip' and selected_pollutant != 'Skip':
+        result = df.xs((selected_country, selected_pollutant), level=("country_name_en","measurements_parameter"))
+    elif selected_pollutant != 'Skip':
+        result = df.xs(selected_pollutant, level="measurements_parameter")
+    elif selected_country != 'Skip':
+        result = df.xs(selected_country, level="country_name_en")
+    else: continue
+
+    result.to_csv("results/search_result.csv")
+    print("Search results stored to results folder")
